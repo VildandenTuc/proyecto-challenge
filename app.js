8636a947bc1e50877465ee2a46db96b9511132ad
VildandenTuc <profesoralbarracin@gmail.com>
2024-08-18 21:18:07 -0300

    update

--- app.js
+++ app.js
@@ -1,4 +1,39 @@
-function encriptar()
-{console.log("OK");
-  return;
+/* La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat" */
+
+let textArea = document.getElementById("container-texto");
+let mensaje = document.getElementById("container-resultado-texto"); 
+
+
+function btnEncriptar(){
+  const textoEncriptado = encriptar(textArea.value);
+  mensaje.value = textoEncriptado;
 }
+
+function encriptar(cadena){
+  const reemplazos = {
+    'a': 'ai',
+    'e': 'enter',
+    'i': 'imes',
+    'o': 'ober',
+    'u': 'ufat'
+  };
+  let cadenaEncriptada = '';
+
+   for (let i = 0; i < cadena.length; i++) {
+      let letra = cadena[i];
+      
+      if (reemplazos[letra]) {
+          cadenaEncriptada += reemplazos[letra];
+      } else {
+          
+          cadenaEncriptada += letra;
+      }
+  }
+  return cadenaEncriptada;
+}
+
+
